ea097032b43820595a9dd8dbb469276afac8a485
Cionk <herr.ahoffmann@gmx.de>
2016-07-29 20:27:28 +0200

    1.5.1

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -29,7 +29,7 @@ Imports System.Runtime.InteropServices
 '
 ' Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 ' übernehmen, indem Sie "*" eingeben:
-<Assembly: AssemblyVersion("1.5")>
+<Assembly: AssemblyVersion("1.5.1")>
 
 '<Assembly: AssemblyVersion("0.0.0")> 
 '<Assembly: AssemblyFileVersion("0.0.0.0")> 
