59b0d27516797946c1663c271f27383810a82342
Andreas Hoffmann <herr.ahoffmann@gmx.de>
2017-11-20 17:07:45 +0100

    v1.6.0

--- Gui/FormInfo.vb
+++ Gui/FormInfo.vb
@@ -3,7 +3,7 @@
         Private Sub FormInfo_Shown(sender As Object, e As EventArgs) Handles Me.Shown
             Dim info As New Text.StringBuilder()
 
-            info.AppendFormat($"xScreen {Application.ProductVersion} by Andreas Hoffmann (xscreen.codeplex.com)")
+            info.AppendFormat($"xScreen {Application.ProductVersion} by Andreas Hoffmann (https://github.com/Qomaa/xScreen)")
             info.AppendLine()
             info.AppendLine("Icons by INCORS GmbH (www.incors.com)")
             info.AppendLine("Ionic.Zip.dll: (dotnetzip.codeplex.com)")
